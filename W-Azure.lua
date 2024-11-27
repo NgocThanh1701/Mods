cf9a6c7ec924306701b6e51d63c3ca0f7ebd8af7
NgocThanh1701 <151608497+NgocThanh1701@users.noreply.github.com>
2024-11-27 19:36:24 +0700

    W-Azure.lua

--- W-Azure.lua
+++ W-Azure.lua
@@ -1,36 +1,4 @@
 script_key = "GeBtsYYUVzRVJkJfwouBINmjlIUqNkfN"
-getgenv().SetFpsCap = false 
-getgenv().FpsCap = 60
-    getgenv().UiCheckItems = false
-    getgenv().WhiteScreen = false
-    getgenv().OneClickSetting = {
-        Enable=true,
-        UnlimitGetQuest=true,
-        TripleQuest=true,
-        AutoAddStats=true,
-        RedeemCode=false,
-        Sea2KeyHop=true,
-        FruitEat = { --Priority, Name, 
-            [1] = {},
-            [2] = {}
-        },
-        EatFruitFromStorage = false,
-        SnipeFruit = true,
-        SnipeFruitMirage = true,
-        HopIfFoundNearExploiter = false,
-        HopHakiColor =false,
-        HopTushita = false,
-        HopValkyriehelm = false,
-        HopMirrorFractal=false,
-        FarmPole = false, -- Turn off If Want Focus Level
-        FarmItems = true, --Turn off If Want Focus Level And CDK, THis Only get After You Get God Human
-        DisableSoulGuitar = false,
-        DisableCDK = false,
-        DisableRaceEvolve = false,
-         RollRace={
-            Enable = false,
-            Races = {"Mink","Fishman","Human","Skypiea"}
-        },
-       MinFragment = 20000, --Farm After Max Level
-    }
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vinhuchi/rblx/main/W-azure/BloxFruit/OneClickLoader.lua"))()
+getgenv().FixCrash = false -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
+getgenv().FixCrash2 = false -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
