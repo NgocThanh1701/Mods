8dad6138cb1e2b9bba7c299f71df0659a6ae3c9d
NgocThanh1701 <151608497+NgocThanh1701@users.noreply.github.com>
2025-01-31 19:04:52 +0700

    W-Azure.lua

--- W-Azure.lua
+++ W-Azure.lua
@@ -3,3 +3,7 @@ getgenv().FixCrash = false -- Turn it On For Hopping Server, Improve Performance
 getgenv().FixCrash2 = false -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
 print("Successfully loaded script")
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Successfully";
+	Text = "loaded script";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
